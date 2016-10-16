1bd57a70e29303c5e8688114c853d5231431c223
Lily Li <ylilyli24@gmail.com>
2016-10-16 14:15:31 -0700

    parse through paragraph text

--- parser.py
+++ parser.py
@@ -1,1 +1,11 @@
-s
+import csv
+
+with open('debate.csv', 'rt') as f, open('text.csv', 'wt') as g:
+    reader = csv.reader(f)
+    writer = csv.writer(g)
+    for i, speaker, text, time in reader:
+        row = text.split(' ')
+        row.insert(0, speaker)
+        writer.writerow(row)
+
+
